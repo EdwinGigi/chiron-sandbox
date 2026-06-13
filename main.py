a44bc13a6cc8e70217a6d8f17f283163398e3a37
Edwin Gigi <edwingigi2012@gmail.com>
2026-06-13 11:38:24 +0100

    Introduce a deliberate bug

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os  # Unused import (Ruff will hate this)
+
 def hello_world():
-    print("Hello, world!")
+   print("Hello, world!") # Bad indentation or logic
 
 if __name__ == "__main__":
     hello_world()
